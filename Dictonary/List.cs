7814ae6e252ce541065ee67a86ab794824a6ae4a
Fatih Cevizoğlu <fatihcevizoglu@hotmail.com>
2021-02-13 15:00:00 +0300

    Proje dosyası ekle.

---
+++ Dictonary/List.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictonary
+{
+    class List<F>
+    {
+        F[] items;
+
+        public List()
+        {
+            items = new F[0];
+        }
+        public void Add(F item)
+        {
+            F[] tempArray = items;
+            items = new F[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+
+        }
+
+    }
+}
---
+++ Dictonary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> kayit = new List<string>();
+            kayit.Add("Ali");
+            kayit.Add("Veli");
+            kayit.Add("Ayşe");
+        }
+    }
+}
